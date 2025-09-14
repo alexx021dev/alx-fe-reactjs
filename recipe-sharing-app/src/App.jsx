4e4b9f436bbecb6f235e147e59eac3df89011baa
alexx021dev <taddelealex0@gmail.com>
2025-09-14 18:56:06 +0300

    Added existing React app into repo

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
@@ -6,27 +6,29 @@ import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div>
-      <h1>🍲 Recipe Sharing App</h1>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
+    <Router>
+      <div>
+        <h1>🍲 Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
 
-      <SearchBar />
+        <SearchBar />
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
