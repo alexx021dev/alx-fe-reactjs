be943a2fe9ee9815c0733689a0f8a6dbb1ec3430
alexx021dev <taddelealex0@gmail.com>
2025-10-05 14:35:15 +0300

    working with tailwind

---
+++ tailwind-react-integratio/src/App.jsx
@@ -0,0 +1,16 @@
+import UserProfile from './components/UserProfile';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <UserProfile />
+    </>
+  )
+}
+
+  
+
